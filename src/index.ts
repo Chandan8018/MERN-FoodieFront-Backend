da1ea1cdbb65e968c4c7be0567a3b201423d986c
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-04-29 00:15:10 +0530

    connection build with mongodb atlas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MANGO_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to MongoDB!!"))
+  .catch((err) => console.log(err));
 
 const app = express();
 app.use(cors());
