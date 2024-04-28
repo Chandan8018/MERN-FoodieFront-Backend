c41df5eccb016dc3edc9646e8aecd70c25d28954
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-04-28 23:22:13 +0530

    complete Setup and test api call

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", async (req: Request, res: Response) => {
+  res.json({message: "Hello World"});
+});
+
+app.listen(4000, () => console.log("Server started on port 4000"));
